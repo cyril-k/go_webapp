19b01c49402a073ab169ee3c697ee6e7e417e3c3
kkondratenko <kkondratenko@assystem.com>
2023-10-15 11:19:36 +0200

    add routing and sessions

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
+	"github.com/alexedwards/scs/v2"
 	"github.com/kkn1993/go_webapp/pkg/config"
 	"github.com/kkn1993/go_webapp/pkg/handlers"
 	"github.com/kkn1993/go_webapp/pkg/render"
@@ -12,8 +14,21 @@ import (
 
 const portNumber = ":8088"
 
+var app config.AppConfig
+var session *scs.SessionManager
+
 func main() {
-	var app config.AppConfig
+
+	// TODO change to true when in prod
+	app.InProduction = true
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
 
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
@@ -28,9 +43,17 @@ func main() {
 
 	render.NewTemplates(&app)
 
-	http.HandleFunc("/", handlers.Repo.Home)
-	http.HandleFunc("/about", handlers.Repo.About)
+	// http.HandleFunc("/", handlers.Repo.Home)
+	// http.HandleFunc("/about", handlers.Repo.About)
 
 	fmt.Printf("Startin application on port %s", portNumber)
-	_ = http.ListenAndServe(portNumber, nil)
+	// _ = http.ListenAndServe(portNumber, nil)
+
+	srv := &http.Server{
+		Addr:    portNumber,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	log.Fatal(err)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/kkn1993/go_webapp
 
 go 1.21.1
+
+require github.com/go-chi/chi/v5 v5.0.10
+
+require (
+	github.com/alexedwards/scs/v2 v2.5.1 // indirect
+	github.com/justinas/nosurf v1.1.1 // indirect
+)
